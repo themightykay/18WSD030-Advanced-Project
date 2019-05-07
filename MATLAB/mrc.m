91b792d6413ac8951e0c1e1087cb37f9b9349ed6
Kyp Diamantides <kypdia@hotmail.com>
2019-05-07 16:58:41 +0100

    Updated COPYRIGHT notice

--- MATLAB/mrc.m
+++ MATLAB/mrc.m
@@ -1,6 +1,6 @@
 %    MRC MATLAB function combines two signals using Equal Gain Combining divesity technique, this is done by adding their
 %squares and then getting the square root.
-%    Copyright (C) 2018  K. Dimantides
+%    Copyright (C) 2018  K. Diamantides
 %
 %    This program is free software: you can redistribute it and/or modify
 %    it under the terms of the GNU Affero General Public License as published
