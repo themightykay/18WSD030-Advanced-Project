0721609bc00e0360c2e8d16c238b014fe7c84144
Kyp Diamantides <kypdia@hotmail.com>
2019-05-07 16:52:53 +0100

    Inserted COPYRIGHT notice

--- MATLAB/egc.m
+++ MATLAB/egc.m
@@ -1,3 +1,20 @@
+%    EGC MATLAB function combines two signals using Equal Gain Combining divesity technique, this is done by adding their
+%magnitudes and then dividing them by the number of signals.
+%    Copyright (C) 2018  K. Dimantides
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU Affero General Public License as published
+%    by the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU Affero General Public License for more details.
+%
+%    You should have received a copy of the GNU Affero General Public License
+%    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 function [combined_signal] = egc(channel_1_power,channel_2_power)
 %Equal gain combining function combines two signals by adding their
 %magnitudes and then dividing them by the number of signals.
